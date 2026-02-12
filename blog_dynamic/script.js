c7eb3aed6e30188377eac1fb6e8a9ca9f779c19d
rain-35 <113638519+rain-35@users.noreply.github.com>
2026-02-12 09:39:23 -0700

    started books

---
+++ blog_dynamic/script.js
@@ -0,0 +1,70 @@
+
+const articles = [
+	{
+		id: 1,
+		title: 'Septimus Heap Book One: Magyk',
+		date: 'July 5, 2022',
+		description:
+			'If you enjoy stories about seventh sons of seventh sons and magyk this is the book for you.',
+		imgSrc: 'https://upload.wikimedia.org/wikipedia/en/5/5f/Magkycover2.jpg',
+		imgAlt: 'Book cover for Septimus Heap 1',
+		ages: '10-14',
+		genre: 'Fantasy',
+		stars: '⭐⭐⭐⭐'
+	},
+	{
+		id: 2,
+		title: 'Magnus Chase Book One: Sword of Summer',
+		date: 'December 12, 2021',
+		description:
+			'The anticipated new novel by Rick Riordan. After Greek mythology (Percy Jackson), Greek/Roman (Heroes of Olympus), and Egyptian (Kane Chronicles), Rick decides to try his hand with Norse Mythology, and the end result is good.',
+		imgSrc:
+			'https://books.google.com/books/content/images/frontcover/xWuyBAAAQBAJ?fife=w300',
+		imgAlt: 'Book cover for Magnus Chase 1',
+		ages: '12-16',
+		genre: 'Fantasy',
+		stars: '⭐⭐⭐⭐'
+	},
+	{
+		id: 3,
+		title: "Belgariad Book One: Pawn of Prophecy",
+		date: "Feb 12, 2022",
+		description:
+		"A fierce dispute among the Gods and the theft of a powerful Orb leaves the World divided into five kingdoms. Young Garion, with his 'Aunt Pol' and an elderly man calling himself Wolf --a father and daughter granted near-immortality by one of the Gods -- set out on a complex mission.",
+		imgSrc:
+		"https://images-na.ssl-images-amazon.com/images/I/41ZxXA+nInL.jpg",
+		imgAlt: "Book cover for Pawn of Prophecy",
+		ages: "12-16",
+		genre: "Fantasy",
+		stars: "⭐⭐⭐⭐⭐"
+	}
+];
+
+articles.foreach(setBook);
+
+function setBook(book){
+    const article = document.createElement('article');
+    article.className = 'book';
+    let html = `
+                <div id="book_info">
+                <p id="date">${book.date}</p>
+                <p>${book.ages}</p>
+                <p>${book.genre}</p>
+                <p aria-label="${book.stars.length} out of five stars" aria-hidden>${book.stars}</p>
+                </div>
+
+            <hr class="vertical-line">   
+
+            <h2>${book.title}</h2>
+
+            <img id="book_cover" src="${book.imgSrc}" alt="${book.imgAlt}">
+
+            <p class="description">${book.description}</p>
+
+`
+    article.innerHTML = html;
+    section.appendChild(article);
+
+}
+
+
